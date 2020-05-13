2b1e4f40eac727a1baa08c52c1c942adfdb7f02f
Jinpei Chen <jinpei.chen@anu.edu.au>
2020-05-13 20:12:17 +1000

    Update test.java

--- src/test.java
+++ src/test.java
@@ -8,12 +8,17 @@ import java.time.format.DateTimeFormatter;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 import java.util.regex.*;
 import java.util.stream.Collectors;
 import java.util.Date;
 
 public class test {
-   public static void main(String[] args) throws ParseException, IOException {
+   final private static String host = "comp3310.ddns.net";
+   final private static int port = 7880;
+   final private static int WAIT_TIME = 0;
+
+   public static void main(String[] args) throws ParseException, IOException, InterruptedException {
       // String str = "<a href=\"B/20.html\">Here's a page</a>  <a href=\"A/20.html\">Here's a page</a>  <a href=\"C/20.html\">Here's a page</a>";
       // String str1 = "HTTP/1.1 200 OKDate: Fri, 08 May 2020 09:06:34 GMTServer: Apache/2.4.29 (Ubuntu)Last-Modified: Sat, 04 May 2019 10:50:04 GMTETag: \"b26-5880da133a5ce\"Accept-Ranges: bytesContent-Length: 2854; Vary: Accept-Encoding";
       // Pattern p = Pattern.compile("Content-Length: (.+?); ", Pattern.DOTALL);
@@ -43,11 +48,11 @@ public class test {
       // System.out.println(dates);
       // System.out.println(date.compareTo(date1));
 
-      try {Socket s = new Socket("www.canberratimes.com.au", 80);
-      } catch (UnknownHostException e) {
-         System.out.println("Uknown Host!!");
-      }  
-         System.out.println("yes");
+      // try {Socket s = new Socket("www.canberratimes.com.au", 80);
+      // } catch (UnknownHostException e) {
+      //    System.out.println("Uknown Host!!");
+      // }  
+      //    System.out.println("yes");
       
       
       // BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
@@ -59,5 +64,48 @@ public class test {
       // BufferedReader bf = new BufferedReader(new InputStreamReader(s.getInputStream()));
       // String t;
       // while((t = bf.readLine()) != null) System.out.println(t);
+
+         String url = "comp3310.ddns.net:7880/A/15.html";
+         try {
+            // Connect to host:port via TCP
+            Socket s = new Socket(host, port);
+            // send GET request
+            url = uTrim(url);
+            String path = url.substring(22);
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
+            bw.write("GET " + "/" + path + " HTTP/1.0\r\n\r\n");
+            bw.flush();
+
+            // start read
+            BufferedReader bf = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            // Convert the buffered reader to string
+            String content = bf.lines().collect(Collectors.joining(";\n "));
+            bf.close();
+            System.out.println("URL contents retrieved: " + url);
+            s.close();
+            System.out.println(content);
+            TimeUnit.SECONDS.sleep(WAIT_TIME);
+
+      } catch (IOException e) {
+            e.printStackTrace();
+      }
+      
    }
+
+   // URL trim
+   public static String uTrim(String url){
+      // remove "http://", add host and port
+      if (url.contains("http://")) url = url.substring(7);
+      else if (url.contains("https://")) url = url.substring(7);
+      else {
+          if (!url.contains(host)){
+              if (url.substring(0,1).equals("/")){
+                  url = host + ":" + port + url;
+              } else url = host + ":" + port + "/" + url;
+          }
+      }
+      if (url.substring(url.length()-1).equals("/")) url = url.substring(0,url.length()-1);
+      
+      return url;
+  } 
 }
